e6fbca6e307279bc196ad12231a917765e6f15aa
ayomikun olamide <ayomikungbada@gmail.com>
2025-02-18 23:26:16 +0100

    pushing to render

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -o errexit  
-pip install -r requirements.txt 
-python manage.py collectstatic   
+pip install -r requirements.txt    
 
 
 python manage.py makemigrations  
 python manage.py migrate  
+
